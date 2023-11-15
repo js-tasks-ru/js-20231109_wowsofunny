8ca06de0395e3dbda3e10e5f39a52d8a0582fba4
Pavel <pavel_9_5@mail.ru>
2023-11-15 17:41:22 +0300

    Add 02-javascript-data-types_2-pick task

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+  let newObj = {};
 
+  for (let field of fields) {
+    if (obj[field]) {
+      newObj[field] = obj[field];
+    }
+  }
+
+  return newObj;
 };
