ff4cff717d241e3fec8ebf7bdd09985b6834493b
Pavel <pavel_9_5@mail.ru>
2023-11-16 17:53:04 +0300

    Add 03-objects-arrays-intro-to-testing_2-invert-object task

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,21 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (obj && Object.keys(obj).length) {
+    const newObj = { ...obj };
+    const objEntriesArr = Object.entries(newObj);
 
+    for (const item of objEntriesArr) {
+      let temp = item[0];
+
+      item[0] = item[1];
+      item[1] = temp;
+    }
+
+    return Object.fromEntries(objEntriesArr);
+  } else if (obj && !Object.keys(obj).length) {
+    return {};
+  } else {
+    return undefined;
+  }
 }
